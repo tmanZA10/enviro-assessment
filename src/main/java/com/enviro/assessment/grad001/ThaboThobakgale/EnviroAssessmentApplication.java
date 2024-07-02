74836c77a143e5cb1b9cf55d04cbf057cd1b97db
Thabo Thobakgale <tthobakgale023a@student.wethinkcode.co.za>
2024-07-02 03:21:39 +0200

    First commit

---
+++ src/main/java/com/enviro/assessment/grad001/ThaboThobakgale/EnviroAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.ThaboThobakgale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnviroAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnviroAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enviro/assessment/grad001/ThaboThobakgale/EnviroAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.ThaboThobakgale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnviroAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
